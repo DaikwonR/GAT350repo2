1120d03b213d96b286546ffbe50f62162390e371
DaikwonR <157632694+DaikwonR@users.noreply.github.com>
2024-11-07 10:47:32 -0700

    11/6
    forgot to update

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
     // render scene
     // framebuffer.Clear(ColorConvert(color4_t{ 0.25f, 0, 0, 1 }));
     scene.Update();
-    scene.Render(framebuffer, camera, 2, 3);
+    scene.Render(framebuffer, camera, 195, 6);
     framebuffer.Update();
 
 #pragma endregion
@@ -141,34 +141,44 @@ void InitScene(Scene& scene)
 void InitScene02(Scene& scene, Camera& camera)
 {
     camera.SetFOV(40.0f);
-    camera.SetView({ 0, 5, -10 }, { 0, 5, 0 });
+    camera.SetView({ 0, 5, -20 }, { 0, 5, 0 });
 
-    std::shared_ptr<Material> material = std::make_shared<Lambertian>(color3_t{ 1, 1, 1 });
-    auto object = std::make_unique<Sphere>(glm::vec3{ 0, 0, 0 }, 2.0f, material);
+    std::shared_ptr<Material> material = std::make_shared<Lambertian>(color3_t{ 1.8f, 0.2f, 0.5f });
+    auto object = std::make_unique<Sphere>(glm::vec3{ 0, 0, 8 }, 2.0f, material);
     scene.AddObject(std::move(object));
 
 
     std::shared_ptr<Material> grey = std::make_shared<Metal>(color3_t{0.5f}, 0.0f);
     std::shared_ptr<Material> red = std::make_shared<Lambertian>(color3_t{ 1, 0, 0 });
     std::shared_ptr<Material> blue = std::make_shared<Emissive>(color3_t{ 0, 0, 1 }, 1.0f);
-    std::shared_ptr<Material> idk = std::make_shared<Dielectric>(color3_t{ 0, 1, 1 }, 20.4f);
+    std::shared_ptr<Material> white2 = std::make_shared<Emissive>(color3_t{ 1, 1, 1 }, 1.0f);
+    std::shared_ptr<Material> idk = std::make_shared<Dielectric>(color3_t{ 0, 0, 0 }, 25.4f);
     std::shared_ptr<Material> blu = std::make_shared<Lambertian>(color3_t{ 0.5, 0, 1 });
     std::shared_ptr<Material> white = std::make_shared<Lambertian>(color3_t{ 1, 1, 1 });
 
-    std::vector<std::shared_ptr<Material>> materials = { blu, idk };
+    std::vector<std::shared_ptr<Material>> materials = { grey, idk };
 
-    auto plane1 = std::make_unique<Plane>(Transform{ glm::vec3{ 10, 0, 0}, glm::vec3{ 0, 0, 90} }, white);
+    auto plane1 = std::make_unique<Plane>(Transform{ glm::vec3{ 10, 0, 0}, glm::vec3{ 0, 0, 90} }, idk);
     auto plane2 = std::make_unique<Plane>(Transform{ glm::vec3{ -10, 0, 0}, glm::vec3{ 0, 0, 270} }, red);
-    auto plane3 = std::make_unique<Plane>(Transform{ glm::vec3{ 0, 10, 0}, glm::vec3{ 0, 0, 180} }, white);
+    auto plane3 = std::make_unique<Plane>(Transform{ glm::vec3{ 0, 10, 0}, glm::vec3{ 0, 0, 180} }, blue);
     auto plane4 = std::make_unique<Plane>(Transform{ glm::vec3{ 0, -10, 0}, glm::vec3{ 0, 0, 0} }, grey);
     scene.AddObject(std::move(plane1));
     scene.AddObject(std::move(plane2));
     scene.AddObject(std::move(plane3));
     scene.AddObject(std::move(plane4));
 
-    auto model = std::make_unique<Model>(Transform{ glm::vec3{ 0,4,0 } }, materials[random((int)materials.size())]);
+
+    for (int spheres = 0; spheres < 3; spheres++)
+    {
+    auto quad = std::make_unique<Model>(Transform{ glm::vec3{ 0, 10, 0 }, glm::vec3{ 0, 0, 180} }, white2);
+    quad->Load("quad.obj");
+    scene.AddObject(std::move(quad));
+
+    }
+    auto model = std::make_unique<Model>(Transform{ glm::vec3{ 0, 0, 0 } }, white2);
     model->Load("cube.obj");
     scene.AddObject(std::move(model));
+    
     
 }
 
